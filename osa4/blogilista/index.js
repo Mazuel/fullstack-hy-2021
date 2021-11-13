a03d4974ec769f8967e473298e188374c6e9d575
Masa Petra <mvpetra@gmail.com>
2021-11-14 00:52:03 +0200

    Part 4.2

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,46 +1,10 @@
+const app = require('./app'); // varsinainen Express-sovellus
 const http = require('http');
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
 
-app.use(cors());
-app.use(express.json());
+const server = http.createServer(app);
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
-
-if (process.argv.length < 3) {
-  console.log('give password as argument');
-  process.exit(1);
-}
-
-const password = process.argv[2];
-
-const mongoUrl = `mongodb+srv://fullstack:${password}@cluster0.v6i6b.mongodb.net/bloglist?retryWrites=true&w=majority`;
-mongoose.connect(mongoUrl);
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
