c620afa7da852f919ec23b991a4fb44401d2a565
Masa Petra <mvpetra@gmail.com>
2021-11-14 02:02:01 +0200

    Parts 4.5-4.7

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 const dummy = (blogs) => {
   // ...
   return 1;
@@ -10,7 +12,36 @@ const totalLikes = (blogs) => {
   return sum;
 };
 
+const favoriteBlog = (blogs) => {
+  return _.maxBy(blogs, 'likes');
+};
+
+const mostLikes = (blogs) => {
+  const groupedBlogs = _(blogs)
+    .groupBy('author')
+    .mapValues((groupedBlogs) => ({
+      author: groupedBlogs[0].author,
+      likes: _.sumBy(groupedBlogs, 'likes'),
+    }))
+    .value();
+  return _.maxBy(_.values(groupedBlogs), 'likes');
+};
+
+const mostBlogs = (blogs) => {
+  const groupedBlogs = _(blogs)
+    .groupBy('author')
+    .mapValues((groupedBlogs) => ({
+      author: groupedBlogs[0].author,
+      blogs: groupedBlogs.length,
+    }))
+    .value();
+  return _.maxBy(_.values(groupedBlogs), 'blogs');
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
 };
